4ee28ccccf599f6ad19c65d269d63f1c50fd8ebd
pinguill <joansaske07@hotmail.com>
2025-07-29 22:46:00 -0500

    Cambios en la logica

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ let listAmigos = [];
 // Funcion para recorrer la lista de amigos y mostrarlos en el html
 function recorrerLista(){
    document.getElementById("listaAmigos").innerHTML = "";
-   listAmigos.map((amigo) => {
+   for(let i = 0; i < listAmigos.length; i++){
       let li = document.createElement("li");
-      li.textContent = amigo;
+      li.textContent = listAmigos[i];
       document.getElementById("listaAmigos").appendChild(li);
-   })
+   }
 }
 
 // Funcion para agregar amigos a la lista, si el input esta vacio, se muestra un alert
